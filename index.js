f921a112267023aa99e765b3af3d15e39dd24694
+ <jkunins95@gmail.com>
2023-03-07 13:18:46 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "sally";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'char';
+}
